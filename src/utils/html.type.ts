ec057f36bdcb05a4de57dc02026875c4522814b7
Michael Braun <michael@web4brauns.de>
2025-09-01 22:58:09 +0200

    Fix type

--- src/utils/html.type.ts
+++ src/utils/html.type.ts
@@ -64,18 +64,9 @@ export type TImageBlock = {
   };
 };
 
-type CustomBlockLetter =
-  | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm'
-  | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
-  | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M'
-  | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z'
-  | '_' | '-';
-
-type CustomBlockLetterString = `${CustomBlockLetter}${CustomBlockLetterString}` | `${CustomBlockLetter}`;
-
 export type TCustomBlock = {
   id: string;
-  type: `$custom/${CustomBlockLetterString}`;
+  type: `$custom/${ReturnType<typeof crypto.randomUUID>}`;
   data?: unknown;
 };
 
